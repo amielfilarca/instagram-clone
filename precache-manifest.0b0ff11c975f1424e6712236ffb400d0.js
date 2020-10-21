bb2e580c9d84364a1e3618f56df6af4c07f09d57
Amiel Filarca <amielfilarca@gmail.com>
2020-10-21 16:32:59 +0800

    Updates

---
+++ precache-manifest.0b0ff11c975f1424e6712236ffb400d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203d6de02222661755994578d8521e0c",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "8448440122a439763d68",
+    "url": "/instagram-clone/static/css/main.41e4facd.chunk.css"
+  },
+  {
+    "revision": "54f44f76638dd4c050b2",
+    "url": "/instagram-clone/static/js/2.c82b0221.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/instagram-clone/static/js/2.c82b0221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8448440122a439763d68",
+    "url": "/instagram-clone/static/js/main.3edd9cdc.chunk.js"
+  },
+  {
+    "revision": "9d73230a1dae251b7590",
+    "url": "/instagram-clone/static/js/runtime-main.8aef135a.js"
+  }
+]);
